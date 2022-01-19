af5bc18275ae7e60c0a50dfddb8f7644a02534fe
Raimondas Sileikis <raimondas.sileikis@gmail.com>
2022-01-19 14:44:03 +0200

    02-uzduotis

---
+++ js/02-uzduotis.js
@@ -0,0 +1,61 @@
+console.log(`--------02 Uzduotis. Veiksmai su kintamaisiais-------------`);
+
+const pirmas = 2;
+console.log(pirmas);
+
+const antras = 5;
+console.log(antras);
+
+const trecias = 9;
+console.log(trecias);
+
+const skaiciuSuma = pirmas + antras + trecias;
+console.log(skaiciuSuma);
+
+
+
+const ketvirtas = 'Saule';
+console.log(ketvirtas);
+
+const penktas = 'Menulis';
+console.log(penktas);
+
+const sestas = 'Zvaigzde';
+console.log(sestas);
+
+const dangus = `${ketvirtas} ${penktas} ${sestas}`
+console.log(dangus);
+
+
+
+const laipsniai = [2, 6, 8, 4, 7];
+console.log(laipsniai);
+
+const minutes = [8, 3, 5, 10, 21];
+console.log(minutes);
+
+const valandos = [1, 3, 5, 7, 9];
+console.log(valandos);
+
+const laipsniuSuma = laipsniai[0] - laipsniai[1] + laipsniai[2] - laipsniai[3] + laipsniai[4];
+console.log(laipsniuSuma);
+
+const minuciuSuma = minutes[0] - minutes[1] + minutes[2] - minutes[3] + minutes[4];
+console.log(minuciuSuma);
+
+const valanduSuma = valandos[0] - valandos[1] + valandos[2] - valandos[3] + valandos[4];
+console.log(valanduSuma);
+
+
+
+const miestai = [`Vilnius`, `Kaunas`, `Klaipeda`, `Panevezys`,  `Siauliai`];
+console.log(miestai);
+
+const valstybes = [`Lietuva`, `Latvija`, `Estija`, `Lenkija`, `Vokietija`];
+console.log(valstybes);
+
+const zemynai = [`Europa`, `Azija`, `Afrija`, `Amerika`, `Australija`];
+console.log(zemynai);
+
+const salys = `${miestai[0]}, ${miestai[1]}, ${miestai[2]}, ${miestai[3]}, ${miestai[4]}, ${valstybes[0]}, ${valstybes[1]}, ${valstybes[2]}, ${valstybes[3]}, ${valstybes[4]}, ${zemynai[0]}, ${zemynai[1]}, ${zemynai[2]}, ${zemynai[3]}, ${zemynai[4]}`;
+console.log(salys);
