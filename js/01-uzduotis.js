e2b80ef641cd061aedf1f04498995ac2c45da078
Raimondas Sileikis <raimondas.sileikis@gmail.com>
2022-01-19 14:01:51 +0200

    01-uzduotis

---
+++ js/01-uzduotis.js
@@ -0,0 +1,38 @@
+console.log(`--------01-Uzduotis. Kintamuju inicijavimas-------------`);
+
+const pirmas = 2;
+console.log(pirmas);
+
+const antras = 5;
+console.log(antras);
+
+const trecias = 9;
+console.log(trecias);
+
+const ketvirtas = 'Saule';
+console.log(ketvirtas);
+
+const penktas = 'Menulis';
+console.log(penktas);
+
+const sestas = 'Zvaigzde';
+console.log(sestas);
+
+const laipsniai = [2, 6, 8, 4, 7];
+console.log(laipsniai);
+
+
+const minutes = [8, 3, 5, 10, 21];
+console.log(minutes);
+
+const valandos = [1, 3, 5, 7, 9];
+console.log(valandos);
+
+const miestai = [`Vilnius`, `Kaunas`, `Klaipeda`, `Panevezys`,  `Siauliai`];
+console.log(miestai);
+
+const valstybes = [`Lietuva`, `Latvija`, `Estija`, `Lenkija`, `Vokietija`];
+console.log(valstybes);
+
+const zemynai = [`Europa`, `Azija`, `Afrija`, `Amerika`, `Australija`];
+console.log(zemynai);
